5b27ee3f27e2e08742d0a6b0e1c8d1f69a1913e1
Matt Birum <birum18@gmail.com>
2024-05-31 15:09:56 -0500

    call backend from frontend

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,28 @@
-import { ExternalLink } from '@/components/ExternalLink';
+import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 
 
 export default function HomeScreen() {
+
+  const [imageUrl, setImageUrl] = useState<String>("");
+
+  const getImage = async () => {
+    try {
+      const response = await fetch('http://k8s-services-birumhom-35ce0a4c6a-1225687380.us-east-2.elb.amazonaws.com/images/chicago-pixelated');
+      const json = await response.json();
+      setImageUrl(json.data);
+    } catch (error) {
+      console.error(error);
+    } 
+  };
+
+  useEffect(() => {
+    getImage();
+  }, []);
+
   return (
     <div>
-      Hello 2
-      <br/>
-      <ExternalLink href="https://www.google.com/">
-        <span>Go to google</span>
-      </ExternalLink>
+      { imageUrl }
     </div>
   );
 }
